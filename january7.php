f02aec8ff861b97e20249b5b145fb411e6554bab
scheidler <matt@scheidlerwebsolutions.com>
2022-01-07 12:25:40 -0500

    Create january7.php

---
+++ january7.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: January 7
+Plugin URI: https://github.com/
+Description: A sample plugin on January 7
+Author: Scheidler
+Version: 2022.01.07.01
+Author URI: https://github.com
+*/
+
+/* this is a comment.  don't need to commit code since we're just testing.  */
